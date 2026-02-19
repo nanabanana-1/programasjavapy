6ac666d8a40e0cedd9914a324bad098a165b4103
ana vitoria <anavuh134@gmail.com>
2026-02-19 16:07:06 -0300

    Add files via upload

---
+++ Questao1.java
@@ -0,0 +1,12 @@
+//questao 1
+import java.util.Scanner;
+
+public class Questao1{
+
+    public static void main(String[] args) {
+        String nome;
+        Scanner scanner = new Scanner (System.in);
+        nome = scanner.nextLine();
+        System.out.println(nome);
+    }
+}
