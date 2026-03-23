6773923fc51d72a1eac09d5a004ba4f90f9b3be1
ana vitoria <anavuh134@gmail.com>
2026-03-23 16:06:23 -0300

    Add files via upload

---
+++ entrada e saida/Questao4.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Questao4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("digite seu nome: ");
+        String nome = sc.nextLine();
+        System.out.println("Bomdia," + nome);
+    }
+}
