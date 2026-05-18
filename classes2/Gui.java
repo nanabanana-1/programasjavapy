f56917f52e7a1939d1760506d66e5805a7cb386c
ana vitoria (vicky☆) <anavuh134@gmail.com>
2026-05-18 18:24:23 -0300

    Criar Gui.java
    adicionando o modo GUI

---
+++ classes2/Gui.java
@@ -0,0 +1,13 @@
+class Gui implements InterfaceUser{
+    public void mostrarMensagem(String mensagem, String titulo, boolean ehErro) {
+        int tipo = ehErro ? JOptionPane.ERROR_MESSAGE : JOptionPane.INFORMATION_MESSAGE;
+        JOptionPane.showMessageDialog(null, mensagem, titulo, tipo);
+    }
+    public String pedirTexto(String mensagem) {
+        return JOptionPane.showInputDialog(mensagem);
+    }
+    public int pedirInteiro(String mensagem) {
+        String resposta = JOptionPane.showInputDialog(mensagem);
+        return resposta == null ? -1 : Integer.parseInt(resposta);
+    }
+}
